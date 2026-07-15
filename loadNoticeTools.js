759f305ebaa8b0470f6fab08e369c3b812143213
wuwz <73680006+wuwen502316@users.noreply.github.com>
2026-07-15 20:03:18 +0800

    Update loadNoticeTools.js

--- loadNoticeTools.js
+++ loadNoticeTools.js
@@ -1,18 +1,18 @@
-(function(){ 
+(function(){
 //加载css配置
 //https://wuwen502316.github.io/noticeTools/css/pbulic.css;
 
 const URL = "https://wuwen502316.github.io/noticeTools"
-const cssFileName = ["pbulic",'element','button'];
+const cssHref = ["pbulic",'element','button'];
 const cssRel = "stylesheet";
 const cssType = "text/css";
 const head = document.querySelector("head");
 const body = document.querySelector("body");
-for(let i = 0 ; i < cssFileName.length; i++){
+for(let i = 0 ; i < cssHref.length; i++){
 	let link = document.createElement("link");
 	link.rel = cssRel;
 	link.type = cssType;
-	link.href = `${URL}/css/${cssFileName[i]}/.css`;
+	link.href = `${URL}/css/${cssHref[i]}/.css`;
 	head.appendChild(link);
 }
 
